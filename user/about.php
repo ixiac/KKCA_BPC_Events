596b823e3ab22467c48c3d81c43471152b277340
bieners <bienmarlon.qm@gmail.com>
2024-11-10 14:31:14 +0800

    added maps

--- user/about.php
+++ user/about.php
@@ -131,7 +131,16 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
                                 </div>
                             </div>
                         </div>
-
+                    </div>
+                    <div class="col-md-12 mt-5">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="card-title">Our Location</div>
+                            </div>
+                            <div class="card-body">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.479954569451!2d121.04979807548706!3d13.749905197336629!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd0514211aeacd%3A0xdd39b7daba6c1f1d!2sBatangas%20Presbyterian%20Church%20%2F%20King&#39;s%20Kids%20Christian%20Academy!5e0!3m2!1sen!2sph!4v1731211385572!5m2!1sen!2sph" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
